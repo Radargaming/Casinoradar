00497672084ee1fef7481de33e7016b0a6bd17b1
Radargaming <michael1983x@gmail.com>
2026-07-11 00:53:18 -0400

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>CasinoRadar</h1>
+      <p>
+        Find the best online casinos, bonuses and reviews.
+      </p>
+
+      <button>
+        View Casinos
+      </button>
+    </main>
+  );
+}
