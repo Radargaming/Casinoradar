d6257bd7bc78c60ad6197ae2c5eb09b2b88d3708
Radargaming <michael1983x@gmail.com>
2026-07-11 15:00:59 -0400

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "CasinoRadar - Casino Reviews & Bonuses",
+  description:
+    "Casino reviews, bonuses and guides in English, Russian and Spanish."
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
